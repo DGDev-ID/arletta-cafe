bb87d2e295b5d24ec142146892fa03f126ef40ed
Gideon Zz <gilangrakar@gmail.com>
2026-04-14 00:26:53 +0700

    feat: [GR] add list menu in dashboard

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { ArrowLeftRight, BookOpen, Coffee, DollarSign, Folder, History, Inbox, LayoutGrid, Package, Receipt, Send, ShieldCheck, UserCheck, Utensils } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -13,6 +13,83 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         href: '/dashboard',
         icon: LayoutGrid,
+    },
+];
+
+const masterDataNavItems: NavItem[] = [
+    {
+        title: 'Cafe & Table',
+        href: '/master/cafe',
+        icon: Coffee,
+    },
+    {
+        title: 'Unit',
+        href: '/master/unit',
+        icon: Package,
+    },
+    {
+        title: 'Material (Recipe)',
+        href: '/master/material',
+        icon: BookOpen,
+    },
+    {
+        title: 'Menu Category',
+        href: '/master/menu-category',
+        icon: Folder,
+    },
+    {
+        title: 'Menu',
+        href: '/master/menu',
+        icon: Utensils,
+    },
+];
+
+const userMamagementNavItems: NavItem[] = [
+    {
+        title: 'Admin',
+        href: '/user-management/admin',
+        icon: ShieldCheck,
+    },
+    {
+        title: 'Cashier',
+        href: '/user-management/cashier',
+        icon: UserCheck,
+    },
+    {
+        title: 'Backoffice',
+        href: '/user-management/backoffice',
+        icon: Receipt,
+    },
+];
+
+const managementNavItems: NavItem[] = [
+    {
+        title: 'Unit Material Converter',
+        href: '/management/material-converter',
+        icon: ArrowLeftRight,
+    },
+    {
+        title: 'Inbound Material',
+        href: '/management/inbound-material',
+        icon: Inbox,
+    },
+    {
+        title: 'Outbound Material',
+        href: '/management/outbound-material',
+        icon: Send,
+    },
+];
+
+const transactionNavItems: NavItem[] = [
+    {
+        title: 'History Transaction',
+        href: '/transaction/history',
+        icon: History,
+    },
+    {
+        title: 'Cashier',
+        href: '/transaction/cashier',
+        icon: DollarSign,
     },
 ];
 </script>
@@ -32,7 +109,11 @@ const mainNavItems: NavItem[] = [
         </SidebarHeader>
 
         <SidebarContent>
-            <NavMain :items="mainNavItems" :title="'Dashboard'"/>
+            <NavMain :items="mainNavItems" :title="'Dashboard'" />
+            <NavMain :items="masterDataNavItems" :title="'Master Data'" />
+            <NavMain :items="userMamagementNavItems" :title="'User Management'" />
+            <NavMain :items="managementNavItems" :title="'Management'" />
+            <NavMain :items="transactionNavItems" :title="'Transaction'" />
         </SidebarContent>
 
         <SidebarFooter>
