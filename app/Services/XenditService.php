fd8c04db8c5429d012bbfc916f5bee40c0aa7461
Gideon Zz <gilangrakar@gmail.com>
2026-05-29 15:40:43 +0700

    fix: remove paymentId

--- app/Services/XenditService.php
+++ app/Services/XenditService.php
@@ -153,7 +153,7 @@ class XenditService
         Log::info('Xendit Webhook Processed', [
             'reference_id' => $referenceId,
             'status' => $status,
-            'payment_id' => $paymentId
+            // 'payment_id' => $paymentId
         ]);
 
         return true;
