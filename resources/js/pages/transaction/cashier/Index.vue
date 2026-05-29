bc15fccd1dd50502c7d4245a4a18792b556622f2
Gideon Zz <gilangrakar@gmail.com>
2026-05-29 16:32:47 +0700

    fix: promo

--- resources/js/pages/transaction/cashier/Index.vue
+++ resources/js/pages/transaction/cashier/Index.vue
@@ -428,7 +428,7 @@ const printReceiptInline = async (id: number) => {
         enc(printPadRight('Fee', printNumber(Number(trx.fee))) + '\n');
         enc(PRINT_LINE + '\n');
         bytes.push(0x1B, 0x45, 0x01); // bold on
-        if (trx.is_promo) {
+        if (trx.promo_id) {
             const promo = trx.price + trx.fee - Number(trx.total_price);
             enc(printPadRight('Discount', '-' + printNumber(Number(promo))) + '\n');
         }
