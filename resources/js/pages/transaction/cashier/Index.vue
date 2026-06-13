cdb5c274e55ac7b031b6124e8c07858fcabbd609
MochammadAnda <mandafadholli@gmail.com>
2026-06-13 17:29:42 +0700

    fix: print server

--- resources/js/pages/transaction/cashier/Index.vue
+++ resources/js/pages/transaction/cashier/Index.vue
@@ -310,9 +310,7 @@ const printDetailReceiptInline = async (detailId: number) => {
     try {
         const { data: detail } = await axios.get(`/transaction/cashier/detail/${detailId}/receipt-data`);
 
-        const isDesktop = window.innerWidth >= 1024 && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-        
-        if (isDesktop) {
+        if (window.innerWidth >= 1024) {
             const res = await axios.post('http://localhost:3000/print', detail);
             if (res.status === 200 || res.status === 207) {
                 notyf.success('Print sukses');
@@ -371,9 +369,7 @@ const printSelectedDetailReceiptsInline = async () => {
             })
         );
 
-        const isDesktop = window.innerWidth >= 1024 && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-        if (isDesktop) {
+        if (window.innerWidth >= 1024) {
             const res = await axios.post('http://localhost:3000/print', details);
             if (res.status === 200 || res.status === 207) {
                 notyf.success('Print bulk sukses');
@@ -423,9 +419,7 @@ const printReceiptInline = async (id: number, filterType: 'all' | 'FOOD' | 'BEVE
     try {
         const { data: trx } = await axios.get(`/transaction/cashier/${id}/receipt-data`);
 
-        const isDesktop = window.innerWidth >= 1024 && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-        if (isDesktop) {
+        if (window.innerWidth >= 1024) {
             const res = await axios.post('http://localhost:3000/print', trx);
             if (res.status === 200 || res.status === 207) {
                 notyf.success('Print sukses');
