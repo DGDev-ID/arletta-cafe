6c9fcca6f360375942f5337bab18303c69ae6d62
Elang Argawana <elangargawana.15@gmail.com>
2026-08-07 21:07:45 +0700

    fix: permission god

--- give_god_permissions.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
-
-$god = Role::findByName('GOD');
-$god->syncPermissions(Permission::all());
-echo 'Permissions synced to GOD role.';
